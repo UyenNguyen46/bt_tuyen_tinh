a46303e21438421f6860c1b02f689463e89e1547
UyenNguyen46 <ngphuonguyen.46@gmail.com>
2022-10-18 15:24:28 +0700

    thay doi

---
+++ tuyentinh.js
@@ -0,0 +1,12 @@
+function findItemMiss(data) {
+    var i = 1;
+    var arr = [];
+    while (i <= 10) {
+        if (data.indexOf(i) == -1) {
+            arr.push(i);
+        }
+        i++;
+    }
+    return arr;
+}
+console.log(findItemMiss([]));
